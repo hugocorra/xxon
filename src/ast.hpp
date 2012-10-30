2389068bc21cd9db0f59c4e68b180aa748293736
Hugo Juarez Corrá <hugocorra@gmail.com>
2012-10-30 14:32:16 -0200

    added some Visitor classes and removed old code.

--- src/ast.hpp
+++ src/ast.hpp
@@ -24,79 +24,31 @@ namespace xxon
 
     class Dict {
 	public:
-		Dict() : items() {};
+        typedef std::map<std::string, AnyValue> NodeType;
 
-		typedef std::map<std::string, AnyValue> NodeType;
+		Dict() : items() {};
+        size_t size() const { return items.size(); }
         NodeType items;
     };
 
     class List {
 	public:
-		List() : values(0) {};
+        typedef std::vector<AnyValue> NodeType;
 
-		typedef std::vector<AnyValue> NodeType;
-		//typedef std::vector<std::string> NodeType;
-		//typedef int NodeType;
-		//typedef std::vector<int> NodeType;
+		List() : values(0) {};
+        size_t size() const { return values.size(); }
         NodeType values;
     };
 
     class AST {
 	public:
 		AST() : collection(boost::none) {};
-		//AST() : nodes(0) {};
 
-		//typedef std::vector<boost::variant<Dict, List>> NodeType;
-		//typedef std::vector<int> NodeType;
-        //NodeType nodes;
-		typedef boost::variant<boost::none_t, Dict, List> NodeType;
+        bool exists() const { return collection.which() != 0; }
+
+        typedef boost::variant<boost::none_t, Dict, List> NodeType;
 		NodeType collection;
     };
-
-    /* a tree node can be: <string, string> or <string, AST> */
-    //typedef std::map<std::string, ASTNode> NodeType;
-    
-    //class AST;
-    //
-    ///* recursive wrapper, works like a tree */
-    //typedef boost::variant<
-    //    boost::recursive_wrapper<AST>,
-    //    std::string
-    //> ASTNode;
-    //
-    ///* a tree node can be: <string, string> or <string, AST> */
-    //typedef std::map<std::string, ASTNode> NodeType;
-    //
-    //class AST
-    //{
-    //public:
-    //    /* this function is responsible to get the value of a pair<string, string>,
-    //     * inside the map properties. 
-    //     * TODO: I really don't like this method here! In the future, it should be moved inside
-    //     *       the Dispatcher and should work as a lazy function. */
-    //    template <typename T>
-    //    bool getDirective(const std::string& name, T& value_typed)
-    //    {
-    //        try
-    //        {
-    //            auto it = properties.find(name);
-    //            if (it != properties.end())
-    //            {
-    //                // check the value type, and convert it to type T.
-    //                if (std::string* is_string = boost::get<std::string>(&it->second))
-    //                    value_typed = boost::lexical_cast<T>(*is_string);
-    //            }
-    //        }
-    //        catch(boost::bad_lexical_cast &)
-    //        {
-    //            return false;
-    //        }
-
-    //        return true;
-    //    }
-
-    //    NodeType properties;
-    //};
 };
 
 /* in order to use some spirit features, it's necessary to transform it in a random access sequence. */
